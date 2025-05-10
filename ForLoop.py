42fd08a6b7cb3a90a46b7389a1d31eaed61ccc9a
Tridib Sourav <28973825+TridibSourav@users.noreply.github.com>
2025-05-10 19:52:24 +0600

    Update ForLoop.py with comments
    Update ForLoop.py with proper comments

--- ForLoop.py
+++ ForLoop.py
@@ -1,3 +1,19 @@
-Basket=["Banana", "kola", "aam", "Mango"]
-for fruit in Basket:
-    print(fruit)
+OneFruit="Pineapple" 
+for letters in OneFruit:   #for variable in sequence: that;s the sequence.
+    print(letters)      
+#if for the sequence veriable of the code has one string, 
+# it will print all the letters consider each of them as a single value
+Basket=["Banana", "water melon", "orange", "Mango"]
+for fruits in Basket:      
+    print(fruits)
+#if for the sequence veriable of the code has Multiple strings set, 
+# it will print all the strings consider each of them as a single value
+for NumberOnly in range (0,18,3):  
+#range(start, end before, step) it will start print from 0 end before 18 which is 15
+    if NumberOnly == 5: #as step is 3, it will never touch 5 so it won't break
+        break
+    if NumberOnly ==6: #it will skip number 6 bcz of continue statement
+        continue
+    if NumberOnly >= 12: #it will break before the value 12. 12 won't print
+        break
+    print (NumberOnly) 

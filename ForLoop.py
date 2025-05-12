0f8a6d724385dfe9ae3a4ba35b832bbb48786f72
Tridib Sourav <28973825+TridibSourav@users.noreply.github.com>
2025-05-12 17:38:29 +0600

    For Loop with if else statement
    For loop through a List, a string, range(), if, elif, else Statements, Loop control statements (break, pass, continue)

--- ForLoop.py
+++ ForLoop.py
@@ -10,10 +10,17 @@ for fruits in Basket:
 # it will print all the strings consider each of them as a single value
 for NumberOnly in range (0,18,3):  
 #range(start, end before, step) it will start print from 0 end before 18 which is 15
+#if range(18) then it will consider as end before
+# if range(1,18) then it will consider as start, end before.
     if NumberOnly == 5: #as step is 3, it will never touch 5 so it won't break
         break
-    if NumberOnly ==6: #it will skip number 6 bcz of continue statement
+    elif NumberOnly <=3:
+        pass    # Do nothing
+    elif NumberOnly ==6: #it will skip number 6 bcz of continue statement
         continue
-    if NumberOnly >= 12: #it will break before the value 12. 12 won't print
+    elif NumberOnly >= 12: #it will break before the value 12. 12 won't print
         break
-    print (NumberOnly) 
+    else:
+        print("Else never takes conditions") 
+    #because number 6 is continue step so that step will come to else statement
+    print ("You Can print text as well",NumberOnly) 

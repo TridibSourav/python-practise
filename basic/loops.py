d80077cced499d66644f9da144725b52d1208458
Tridib Sourav <28973825+TridibSourav@users.noreply.github.com>
2026-06-02 12:13:11 +0600

    Create loops.py with loop examples
    Added examples of for and while loops, along with list iteration.

---
+++ basic/loops.py
@@ -0,0 +1,25 @@
+# loops.py
+
+# For loop example
+
+print("For Loop:")
+
+for i in range(1, 6):
+    print(i)
+
+# While loop example
+
+print("While Loop:")
+
+count = 1
+
+while count <= 5:
+    print(count)
+    count += 1
+
+# Loop through a list
+
+fruits = ["Apple", "Banana", "Mango"]
+
+for fruit in fruits:
+    print(fruit)
